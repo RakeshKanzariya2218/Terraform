40ef24a496beb388c56735513ae0a2e056775dd2
RakeshKanzariya2218 <rkanzariya1234@gmail.com>
2025-07-20 07:05:46 +0000

    Day-5-statefilebackend

---
+++ Day-5-Ststefilebackend/terraform.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-0150ccaf51ab55a51"
+instance_type = "t2.micro"
+key_name = "key-1"
+tags = "Terraform-1"
---
+++ Day-5-Ststefilebackend/variables.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+  description = "inerting the ami"
+  type = string
+  default = ""
+}
+variable "instance_type" {
+  description = "select the instance type"
+  type = string
+  default = ""
+}
+variable "key_name" {
+  description = "the public key of a instance"
+  type = string
+  default = ""
+}
+variable "tags" {
+  description = "this is the name of server in aws console"
+  type = string
+  default = ""
+
+}
