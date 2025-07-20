6731307e02b8614fdc41c5de95758f80b6fa8368
Rakesh Kanzariya. <rkanzariya1234@gmail.com>
2025-07-20 17:10:38 +0530

    Update backend.tf

--- Day-5-Ststefilebackend/backend.tf
+++ Day-5-Ststefilebackend/backend.tf
@@ -10,7 +10,9 @@ terraform {
 
 
 
-/*terraform {  
+/*
+
+terraform {  
   backend "s3" {  
     bucket       = "your-terraform-state-bucket"  
     key          = "path/to/your/statefile.tfstate"  
@@ -23,4 +25,43 @@ terraform {
 this is the new update by terraform for the supporting s3  bucket to native lock use this configuraton and
 enable the statelocking of s3 statefiele.
 
+----------------------------------------------------------------------
+
+the old using dynamodb lock file for enablleing the statelocking :
+
+terraform {  
+  backend "s3" {  
+    bucket       = "your-terraform-state-bucket"  
+    key          = "path/to/your/statefile.tfstate"  
+    region       = "us-east-1"  
+    dynamodb_table = "nareshit"   # dyanamodb locking 
+    encrypt      = true  
+   
+  }   
+}
+
+in the dynamodb locking configuration innside aws create a dyanmodb table (nareshit) and give partition key ( lockID ) and done than backend.tf inside 
+done abovee changes and done . statelocking is enable.
+
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
