05ea3df021ab195b088fd012d76702dfe662cbf5
RakeshKanzariya2218 <rkanzariya1234@gmail.com>
2025-07-24 04:11:25 +0000

    modifies-s3-statefile

--- Day-6-statetesting-same-key/backend.tf
+++ Day-6-statetesting-same-key/backend.tf
@@ -2,7 +2,8 @@ terraform {
 
   backend "s3" {
     bucket = "rkanzariya.info"
-    key    = "terraform.tfstate"
+    #key    = terraform.tfstate"
+    key    = "day-6--terraform.tfstate"
     region = "us-east-1"
     encrypt      = true  
     use_lockfile = true 
