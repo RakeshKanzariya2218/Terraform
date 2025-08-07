b6933c953dc85f54213e0fcd7b42155f0e60451a
RakeshKanzariya2218 <rkanzariya1234@gmail.com>
2025-08-07 22:18:40 +0530

    modification

--- day-2-introduction/main.tf
+++ day-2-introduction/main.tf
@@ -1,8 +1,6 @@
 resource "aws_instance" "name" {
-  instance_type = "t2.micro"
-  key_name = "key-1"
-  ami = "ami-0150ccaf51ab55a51"
-  tags = {
-    Name = "terraform"
-  }
-  }
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    
+  
+}
--- day-2-introduction/provider.tf
+++ day-2-introduction/provider.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  
+
+ 
 }
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.3.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.0, < 5.0"
     }
   }
 }
